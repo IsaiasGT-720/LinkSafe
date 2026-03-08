47273ed7dc3d6b05ded8aedf5163e94f419387b7
Isaias Gonzalez <isaiasgonzaleztorres97@gmail.com>
2026-03-08 18:27:37 -0500

    fixed static files

--- LinkSafer/settings.py
+++ LinkSafer/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'LinkSafer.urls'
@@ -126,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
