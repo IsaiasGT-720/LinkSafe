f5bf1dd2aded52b35bbd3eb1131669475311b7dd
Isaias Gonzalez <isaiasgonzaleztorres97@gmail.com>
2026-03-08 18:12:43 -0500

    fixed main route

--- CentralLinks/urls.py
+++ CentralLinks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="login", permanent=False)),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path("dashboard/", views.dashboard, name="dashboard"),
