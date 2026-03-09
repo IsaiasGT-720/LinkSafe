d391fbdc55d9b0251a7b1ef2f82e9a9c71def4d5
Isaias Gonzalez <isaiasgonzaleztorres97@gmail.com>
2026-03-09 08:45:18 -0500

    fixed trust domain

--- LinkSafer/settings.py
+++ LinkSafer/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://linksafer.up.railway.app",
+]
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
